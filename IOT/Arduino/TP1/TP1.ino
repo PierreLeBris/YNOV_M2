cee992255a5acbc6114ddd89c5706495c659cfe3
PierreLeBris <pierre.lebris@outlook.com>
2022-10-13 09:38:07 +0200

    Update TP1.ino

--- IOT/Arduino/TP1/TP1.ino
+++ IOT/Arduino/TP1/TP1.ino
@@ -1,10 +1,15 @@
 void setup() {
 // put your setup code here, to run once:
 Serial.begin(9600);
-Serial.println("Hello World");
 }
 
 void loop() {
 // put your main code here, to run repeatedly:
-Serial.println(" Hello World ");
+for (int var = 0 ; var < 101 ; var++) {
+  if (var == 100){
+   Serial.println("Programme Terminé");
+}
+  Serial.println(var);
+}
+
 }

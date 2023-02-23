ed24f07d4c8f4a79e87f637cbccd8a978760a864
PierreLeBris <pierre.lebris@outlook.com>
2023-02-23 09:19:54 +0100

    oui

--- Docker/Project/appscore/Database/SqlCmdStartup.sh
+++ Docker/Project/appscore/Database/SqlCmdStartup.sh
@@ -1,1 +1,2 @@
-sleep 15s && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Pass@word -d master -i SqlCmdScript.sql
+sleep 25
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Pass@word -d master -i SqlCmdScript.sql
